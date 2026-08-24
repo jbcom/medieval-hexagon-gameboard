b45927d621a571470009bbd7cee154a872ff814d
Jon Bogaty <jon@jonbogaty.com>
2026-08-24 10:05:09 -0500

    chore(docs): remove legacy generated artifacts

--- packages/docs-site/.astro/content-assets.mjs
+++
@@ -1,4 +0,0 @@
-
-import __ASTRO_IMAGE_IMPORT_zfhtA from "../../../public/hero.png?astroContentImageFlag=&importer=src%2Fcontent%2Fdocs%2Findex.mdx";
-export default new Map([["../../../public/hero.png?astroContentImageFlag=&importer=src%2Fcontent%2Fdocs%2Findex.mdx", __ASTRO_IMAGE_IMPORT_zfhtA]]);
-		
